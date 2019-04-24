c6debb65d2a957d7b9aa22e45a1293c7a64f4cd3
ydl <yangdeliang@juwang.cn>
2019-04-24 14:59:34 +0800

    feat: asdfaf adsf asdf

--- setup_git_hook.sh
+++ setup_git_hook.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+REPLACE_CRLF_TO_LF=$(cat $1 | sed 's/[\r\n]/#/g' | tr '\n' '#')
+LEN=$(echo ${REPLACE_CRLF_TO_LF} | wc -m)
 
-
-
-sdf
-asdfasdfasdf
-ddsfsdf
+test "${LEN}" = "$(cat $1 | sed 's/[\r\n]/#/g' | tr '\n' '#' |
+    grep -oP '^(feat|fix|docs|style|refactor|perf|test|chore|revert)(\([^\)]+\))?:[^\n]+(#{4}[^#{4}]+){0,2}' |
+    wc -m)" || {
+    echo >&2 bad commit style "(feat|fix|docs|style|refactor|perf|test|chore|revert)(scope):..."
+    exit 1
+}
